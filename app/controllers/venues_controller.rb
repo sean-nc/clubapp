82370f7581dc856458486834885241ab0263871f
Sean <sean.n.cordes@gmail.com>
2018-10-26 15:28:01 -0400

    created controller for venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,51 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def edit
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to @venue
+      flash[:notice] = 'Venue was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @venue.update(venue_params)
+      redirect_to @venue
+      flash[:notice] = 'Venue was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @venue.destroy
+    redirect_to venues_url,
+    flash[:notice] = 'Venue was successfully destroyed.'
+  end
+
+  private
+    def set_venue
+      @venue = Venue.find(params[:id])
+    end
+
+    def venue_params
+      params.fetch(:venue, {})
+    end
 end
